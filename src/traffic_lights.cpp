ebe4996882f7dd7e31ed368bc0e4a6ce6cf9e821
Bhuvansh Goyal <bhuvansh.iitb@gmail.com>
2026-05-27 12:43:28 +0530

    Added question: traffic lights

---
+++ src/traffic_lights.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+#define ll long long
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int x, n;
+
+  cin >> x >> n;
+
+  set<int> light_positions = {0, x};
+  multiset<int> gaps = {x};
+
+  int p;
+  while (cin >> p) {
+    auto right = light_positions.upper_bound(p);
+    auto left = right;
+    --left;
+
+    gaps.erase(gaps.find(*right - *left));
+    gaps.insert(p - *left);
+    gaps.insert(*right - p);
+
+    light_positions.insert(p);
+
+    auto ans = gaps.end();
+    --ans;
+    cout << *ans << " ";
+  }
+}
