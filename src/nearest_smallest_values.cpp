8bd22dcd93f28d1a0e81eaec957472cad288cccf
Bhuvansh Goyal <bhuvansh.iitb@gmail.com>
2026-05-29 13:00:19 +0530

    Added question: nearest smallest values

---
+++ src/nearest_smallest_values.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+#define ll long long
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  cin >> n;
+
+  vector<int> arr;
+  for (int i = 0; i < n; i++) {
+    int value;
+    cin >> value;
+    arr.push_back(value);
+  }
+
+  stack<int> s;
+
+  for (int i = 0; i < n; i++) {
+    while (!s.empty() && (arr[s.top()] >= arr[i])) s.pop();
+
+    if (s.empty()) {
+      cout << 0 << " ";
+    } else {
+      cout << s.top() + 1 << " ";
+    }
+
+    s.push(i);
+  }
+}
